81c7496d48e3a79bee20d0fa06f2860f0368fbfe
John Wikman <john.wikman@hotmail.com>
2023-11-04 13:10:54 +0100

    Automatically move images to device

--- example-simclr.py
+++ example-simclr.py
@@ -137,13 +137,16 @@ cifar10_train = torchvision.datasets.CIFAR10(
     download=True,
 )
 
+from torch.utils.data.dataloader import default_collate
 cifar10_trainloader = torch.utils.data.DataLoader(
     cifar10_train,
     #simclr_train_dataset,
     batch_size=args.batch_size,
     shuffle=True,
     num_workers=args.workers,
-    pin_memory=True, drop_last=True,
+    collate_fn=lambda x: (tuple(xc.to(args.device) for xc in default_collate(x))),
+    pin_memory=True,
+    drop_last=True,
 )
 
 criterion = nn.CrossEntropyLoss().to(args.device)
@@ -152,8 +155,6 @@ top5_accuracies = []
 losses = []
 train_iterator = tqdm(cifar10_trainloader)
 for images, labels in train_iterator:
-    images = images.to(args.device)
-
     opt.zero_grad()
     logits = model(images)
     loss = criterion(logits, labels)
