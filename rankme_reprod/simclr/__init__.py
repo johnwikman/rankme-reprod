e6b4491c85466ba725136d71c7bd3c79bb17625c
Linus Ostlund <linusostlund2@gmail.com>
2023-11-20 13:06:29 +0100

    Restructure

--- rankme_reprod/simclr/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import simclr, data_aug
-
-__version__ = "1.0.1"
-__source__ = "https://github.com/sthalles/SimCLR"
-__license__ = "MIT"
-__help__ = """
-Contains code adapted from https://github.com/sthalles/SimCLR, slightly
-modified to allow better integration with other training algorithms.
-"""
