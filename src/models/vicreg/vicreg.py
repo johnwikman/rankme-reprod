ca1874fa09fbd42b5a9f0295b882879407e574f5
John Wikman <john.wikman@hotmail.com>
2023-11-20 14:55:18 +0100

    Remove obsolete imports.

--- src/models/vicreg/vicreg.py
+++ src/models/vicreg/vicreg.py
@@ -4,11 +4,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import math
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 
 
